387f4de239bcb0fd28891c668e5015c08306ec1f
Behnam Amiri <behamiridev@gmail.com>
2026-01-07 15:16:41 -0700

    Add status steps

---
+++ internal/engine/types.go
@@ -0,0 +1,11 @@
+package engine
+
+type RunStatus string
+
+const (
+	RunStatusQueued    RunStatus = "queued"
+	RunStatusRunning   RunStatus = "running"
+	RunStatusSucceeded RunStatus = "succeeded"
+	RunStatusFailed    RunStatus = "failed"
+	RunStatusCanceled  RunStatus = "canceled"
+)
