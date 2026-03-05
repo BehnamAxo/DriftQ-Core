e9433b125199bc9f634e9be638a24346f2b48fea
Behnam Amiri <behamiridev@gmail.com>
2026-03-05 15:06:04 -0700

    Avoid unnecessary map allocations and clean empty nested maps

--- internal/broker/retry_state.go
+++ internal/broker/retry_state.go
@@ -7,6 +7,25 @@ type retryStateEntry struct {
 	LastErrorAt time.Time
 }
 
+func (b *InMemoryBroker) getRetryState(topic, group string, partition int) map[int64]*retryStateEntry {
+	byTopic, ok := b.retryState[topic]
+	if !ok {
+		return nil
+	}
+
+	byGroup, ok := byTopic[group]
+	if !ok {
+		return nil
+	}
+
+	byPart, ok := byGroup[partition]
+	if !ok {
+		return nil
+	}
+
+	return byPart
+}
+
 func (b *InMemoryBroker) ensureRetryState(topic, group string, partition int) map[int64]*retryStateEntry {
 	if _, ok := b.retryState[topic]; !ok {
 		b.retryState[topic] = make(map[string]map[int]map[int64]*retryStateEntry)
@@ -24,10 +43,32 @@ func (b *InMemoryBroker) ensureRetryState(topic, group string, partition int) ma
 }
 
 func (b *InMemoryBroker) purgeRetryStateLocked(topic, group string, partition int, ackedOffset int64) {
-	byPart := b.ensureRetryState(topic, group, partition)
+	byPart := b.getRetryState(topic, group, partition)
+	if byPart == nil {
+		return
+	}
+
 	for off := range byPart {
 		if off <= ackedOffset {
 			delete(byPart, off)
 		}
 	}
+
+	if len(byPart) != 0 {
+		return
+	}
+
+	byGroup := b.retryState[topic][group]
+	delete(byGroup, partition)
+	if len(byGroup) != 0 {
+		return
+	}
+
+	byTopic := b.retryState[topic]
+	delete(byTopic, group)
+	if len(byTopic) != 0 {
+		return
+	}
+
+	delete(b.retryState, topic)
 }
